2391e037bd3950942c6854c60bfb142658d2523d
mohamedsillahkanu <114839447+mohamedsillahkanu@users.noreply.github.com>
2025-07-03 06:54:37 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -362,7 +362,7 @@ st.markdown("---")  # Add a horizontal line separator
 st.title("📊 School Based Distribution of ITNs in SL")
 
 # Upload file
-uploaded_file = "SBD_07_02_lastest (1).xlsx"
+uploaded_file = "SBD_lastest_07_03_2025.xlsx"
 if uploaded_file:
     # Read the uploaded Excel file
     df_original = pd.read_excel(uploaded_file)
